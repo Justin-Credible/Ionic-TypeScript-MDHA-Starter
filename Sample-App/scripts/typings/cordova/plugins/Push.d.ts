c1b1810891d236cef08206570db6890ae5fee8b0
Justin Unterreiner <justin.unterreiner@gmail.com>
2015-05-31 10:23:00 -0700

    Updated TypeScript definitions to match sister project

--- Sample-App/scripts/typings/cordova/plugins/Push.d.ts
+++ Sample-App/scripts/typings/cordova/plugins/Push.d.ts
@@ -6,21 +6,18 @@
 // Copyright (c) Microsoft Open Technologies, Inc.
 // Licensed under the MIT license.
 
-/*
 interface Window {
     plugins: Plugins
 }
-*/
 
-//interface Plugins {
-//    /**
-//     * This plugin allows to receive push notifications. The Android implementation uses
-//     * Google's GCM (Google Cloud Messaging) service,
-//     * whereas the iOS version is based on Apple APNS Notifications
-//     */
-//    pushNotification: PushNotification
-//}
-
+interface Plugins {
+    /**
+     * This plugin allows to receive push notifications. The Android implementation uses
+     * Google's GCM (Google Cloud Messaging) service,
+     * whereas the iOS version is based on Apple APNS Notifications
+     */
+    pushNotification: PushNotification
+}
 
 /**
  * This plugin allows to receive push notifications. The Android implementation uses
